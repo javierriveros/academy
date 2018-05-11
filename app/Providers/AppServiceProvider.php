2d74583702c5933d23762bd4e803f0e9f6097695
Javier Riveros <javiiriveros@hotmail.com>
2018-05-10 22:25:06 -0500

    Load assets over https on production again x2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,8 +16,6 @@ class AppServiceProvider extends ServiceProvider
         if (env('APP_ENV') !== 'local') {
             $this->app['request']->server->set('HTTPS', true);
         }
-
-        Schema::defaultStringLength(191);
     }
 
     /**
