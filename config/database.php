a30aa87fae462fd6ce7a663ea1708dfc3490ece1
Javier Riveros <javiiriveros@hotmail.com>
2018-05-21 22:45:42 -0500

    Changed config to heroku

--- config/database.php
+++ config/database.php
@@ -1,15 +1,15 @@
 <?php
-// $url = parse_url(getenv("DATABASE_URL"));
+$url = parse_url(getenv("DATABASE_URL"));
 
-// $host = $url["host"];
-// $username = $url["user"];
-// $password = $url["pass"];
-// $database = substr($url["path"], 1);
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 
-$host = "";
-$username = "";
-$password = "";
-$database = "";
+// $host = "";
+// $username = "";
+// $password = "";
+// $database = "";
 
 return [
 
